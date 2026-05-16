b25ec9024e4adef756e17e552436924110734b0f
AYANA291 <ayana88g@gmail.com>
2026-05-16 17:52:37 +0530

    Update va.py

--- va.py
+++ va.py
@@ -1,84 +1,31 @@
-from gtts import gTTS
-import pyglet
-import time
-import os
-import speech_recognition as sr
 import webbrowser
 import datetime
 import pyjokes
 
-# ---------------- Text-to-Speech ---------------- #
-def speak(text):
-    tts = gTTS(text=text, lang='en')
-    filename = "temp.mp3"
-    tts.save(filename)
-    
-    music = pyglet.media.load(filename, streaming=False)
-    music.play()
-    time.sleep(music.duration)  # wait until finished
-    
-    os.remove(filename)
+print("🧠 TaskMate Started")
 
-# ---------------- Speech-to-Text ---------------- #
-def takeCommand():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
-        r.energy_threshold = 300
-        r.adjust_for_ambient_noise(source, duration=1)
-        audio = r.listen(source)
-    try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}")
-    except:
-        print("Say that again please...")
-        return "none"
-    return query.lower()
+while True:
+    query = input("Enter command: ").lower()
 
-# ---------------- Main Program ---------------- #
-if __name__ == "__main__":
-    speak("Hello! I am your assistant. How can I help you today?")
+    if "youtube" in query:
+        webbrowser.open("https://youtube.com")
+        print("Opening YouTube")
 
-    while True:
-        query = takeCommand()
+    elif "google" in query:
+        webbrowser.open("https://google.com")
+        print("Opening Google")
 
-        if "your name" in query:
-            speak("My name is Peter")
-            print("My name is Peter")
+    elif "time" in query:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print("Current time:", current_time)
 
-        elif "open youtube" in query:
-            webbrowser.open("https://youtube.com")
-            speak("Opening YouTube")
+    elif "joke" in query:
+        print(pyjokes.get_joke())
 
-        elif "open google" in query:
-            webbrowser.open("https://google.com")
-            speak("Opening Google")
+    elif "exit" in query:
+        print("Goodbye 👋")
+        break
 
-        elif "open website" in query:
-            speak("Which website should I open?")
-            site = takeCommand()
-            if site != "none":
-                if not site.startswith("http"):
-                    site = "https://" + site
-                webbrowser.open(site)
-                speak(f"Opening {site}")
-
-        elif "the time" in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"The time is {strTime}")
-            print(strTime)
-
-        elif "tell me a joke" in query:
-            joke = pyjokes.get_joke()
-            speak(joke)
-            print(joke)
-
-        elif "exit" in query or "quit" in query:
-            speak("Goodbye! Have a nice day!")
-            break
-
-        else:
-            speak("I am searching that on Google for you.")
-            webbrowser.open(f"https://www.google.com/search?q={query}")
+    else:
+        print("Searching on Google...")
+        webbrowser.open(f"https://www.google.com/search?q={query}")
